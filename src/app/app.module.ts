a6815a49500723797eb23b80447c16878526bc2a
Mariia Rizhko <ma.rizhko@gmail.com>
2020-04-10 16:35:51 +0300

    Login page and login-title added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginTitleComponent } from './login-title/login-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginTitleComponent
   ],
   imports: [
     BrowserModule
