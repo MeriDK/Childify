9eae96a99a12c687c20eb9b9fc80fdd54eb57b93
Mariia Rizhko <ma.rizhko@gmail.com>
2020-04-10 16:39:44 +0300

    Task components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginTitleComponent } from './login-title/login-title.component';
+import { CheckTaskComponent } from './check-task/check-task.component';
+import { ChildTaskComponent } from './child-task/child-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginTitleComponent
+    LoginTitleComponent,
+    CheckTaskComponent,
+    ChildTaskComponent,
+    TaskListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
