15c179f740c9acb3da8a1d48591ba8d318b687d8
Oleh <olehvel@gmail.com>
2020-04-29 13:39:47 +0300

    clear

--- back-end/app/registration_api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from childify_registry.api.models import User, Family, Child, Parent
-
-admin.site.register(User)
-admin.site.register(Family)
-admin.site.register(Child)
-admin.site.register(Parent)
