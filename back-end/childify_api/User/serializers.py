ed0c262bfed35e1c561f7f54d85480069cb09594
Oleh <olehvel@gmail.com>
2020-04-29 22:12:21 +0300

    +

--- back-end/childify_api/User/serializers.py
+++ back-end/childify_api/User/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ['username', 'email', 'password', 'isParent']
+    fields = all
