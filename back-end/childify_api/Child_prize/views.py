dbc812ca9c7e9593232c5ef312c4e456c2da0d43
Meri <ma.rizhko@gmail.com>
2020-05-04 18:18:41 +0300

    fix family id in prize post

--- back-end/childify_api/Child_prize/views.py
+++ back-end/childify_api/Child_prize/views.py
@@ -1,7 +1,13 @@
+from django.http import JsonResponse
+
 from .models import ChildPrize
 from .serializers import ChildPrizeSerializer
 from rest_framework import viewsets
 from rest_framework import generics
+from rest_framework import mixins
+
+from Prize.models import Prize
+from Child.models import Child
 
 
 class ChildPrizeViewSet(viewsets.ModelViewSet):
@@ -9,7 +15,7 @@ class ChildPrizeViewSet(viewsets.ModelViewSet):
     queryset = ChildPrize.objects.all()
 
 
-class ChildPrizeList(generics.ListAPIView, generics.CreateAPIView):
+class ChildPrizeList(generics.ListAPIView, generics.GenericAPIView, mixins.CreateModelMixin):
     serializer_class = ChildPrizeSerializer
 
     def get_queryset(self):
@@ -21,3 +27,14 @@ class ChildPrizeList(generics.ListAPIView, generics.CreateAPIView):
             if status_id:
                 queryset = queryset.filter(status_id=status_id)
         return queryset
+
+    def post(self, request, *args, **kwargs):
+        serializer_class = ChildPrizeSerializer(data=request.data)
+        if serializer_class.is_valid():
+            child = Child.object.filter(id=request.data['child_id']).first()
+            prize = Prize.objects.filter(id=request.data['prize_id']).first()
+            if child.family.id != prize.family_id.id:
+                return JsonResponse({'message': 'child and prize have different families'}, status=400)
+            queryset = ChildPrize.objects.all()
+            return self.create(request, *args, **kwargs)
+        return JsonResponse(serializer_class.errors, status=400)
