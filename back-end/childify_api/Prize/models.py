90403029a57698b206140e6770ff18d538eedb90
Meri <ma.rizhko@gmail.com>
2020-05-04 16:02:45 +0300

    add about to prize

--- back-end/childify_api/Prize/models.py
+++ back-end/childify_api/Prize/models.py
@@ -6,6 +6,7 @@ class Prize(models.Model):
     family_id = models.ForeignKey(Family, on_delete=models.CASCADE)
     points = models.IntegerField()
     name = models.TextField(max_length=30)
+    about = models.TextField(max_length=500, default='')
 
     def __str__(self):
-        return f'id: {self.id} family_id: {self.family_id} points: {self.points} name: {self.name}'
+        return f'id: {self.id} family_id: {self.family_id} points: {self.points} name: {self.name} about: {self.about}'
