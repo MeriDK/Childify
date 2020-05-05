ede0a29bff1112732fd0bf26be792eb5b5d15a2c
Anastasiia Hrynyshyn <anastasiia.hrynyshyn@gmail.com>
2020-05-05 15:02:16 +0300

    add icon for category in task

--- front-end/src/app/task-add/task-add.component.ts
+++ front-end/src/app/task-add/task-add.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {translate} from '../services/StringResourses'
 
+
 @Component({
   selector: 'app-task-add',
   templateUrl: './task-add.component.html',
   styleUrls: ['./task-add.component.sass']
 })
-export class TaskAddComponent implements OnInit {
+export class TaskAddComponent {
   translate = translate
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(){
+   
   }
+  
 
 }
