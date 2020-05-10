3dc5fbc38e5f12101e6d8c759e1634a61c4c3a0f
Ridiculous_P <yci.cipi@gmail.com>
2020-05-10 11:35:17 +0300

    Basick style

--- front-end/src/app/settings-page/settings-page.component.ts
+++ front-end/src/app/settings-page/settings-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
-  styleUrls: ['./settings-page.component.sass']
+  styleUrls: ['./settings-page.component.scss']
 })
 export class SettingsPageComponent implements OnInit {
 
