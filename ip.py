fdbc7b3848d5690e99af1ffb9cbd1dce21a88e95
Oleh <olehvel@gmail.com>
2020-06-06 18:53:07 +0300

    +

--- ip.py
+++ ip.py
@@ -18,14 +18,3 @@ with open('package.json', 'r+') as f:
     f.seek(0)        
     json.dump(data, f, indent=4)
     f.truncate()     
-
-with open('front-end/package.json', 'r+') as f:
-    data = json.load(f)
-    data['scripts']['front-server'] = "ng serve --host "+ip_address
-    if ip_address=="127.0.0.1":
-        data['scripts']['back-server'] = "..\\back-end\\venv\\Scripts\\activate.bat && python ../back-end/childify_api/manage.py runserver"
-    else:
-        data['scripts']['back-server'] = "..\\back-end\\venv\\Scripts\\activate.bat && python ../back-end/childify_api/manage.py runserver 0.0.0.0:8000 "
-    f.seek(0)        
-    json.dump(data, f, indent=4)
-    f.truncate()     
