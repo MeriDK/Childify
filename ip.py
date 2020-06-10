ba6136105957a11beccd532a74a4f12f43e09dc2
Ridiculous_P <yci.cipi@gmail.com>
2020-06-10 20:29:31 +0300

    Linux npm commands update

--- ip.py
+++ ip.py
@@ -17,10 +17,10 @@ with open('package.json', 'r+') as f:
     data['scripts']['fe-ip'] = "cd front-end && ng serve --host "+ip_address
     if ip_address=="localhost":
         data['scripts']['be-ip'] = "back-end\\venv\\Scripts\\activate.bat && python back-end/childify_api/manage.py runserver"
-        data['scripts']['be-l-ip'] = "source back-end/venv/bin/activate && python back-end/childify_api/manage.py runserver"
+        data['scripts']['be-l-ip'] = "python back-end/childify_api/manage.py runserver"
     else:
         data['scripts']['be-ip'] = "back-end\\venv\\Scripts\\activate.bat && python back-end/childify_api/manage.py runserver 0.0.0.0:8000 "
-        data['scripts']['be-l-ip'] = "source back-end/venv/bin/activate && python back-end/childify_api/manage.py runserver 0.0.0.0:8000 "
+        data['scripts']['be-l-ip'] = "python back-end/childify_api/manage.py runserver 0.0.0.0:8000 "
     data['baseURL']='http://'+ip_address+':8000'
     data['IP']=ip_address
     f.seek(0)        
