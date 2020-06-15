bd95df24594937090499e972468036ddb27719db
Anastasiia Hrynyshyn <anastasiia.hrynyshyn@gmail.com>
2020-06-15 17:59:54 +0300

    new status and time (issues 133 and 134)

--- front-end/src/app/task-check/task-check.service.ts
+++ front-end/src/app/task-check/task-check.service.ts
@@ -20,18 +20,18 @@ export class TaskCheckService {
   }
 
   updateTasktoDone(task): Observable<any> {
-    const body = {name_task: task.name_task ,info_task: task.info_task ,point_task: task.point_task,status:4}
+    const body = {name_task: task.name_task ,info_task: task.info_task ,point_task: task.point_task,status:5}
     return this.http.patch(config['baseURL'] + "/task/"+task.id +"/" ,body,
     {headers: this.httpHeaders})
   }
   addPoint(task):Observable<any> {
-    const body = {name_task: task.name_task ,info_task: task.info_task ,point_task: task.point_task,status:4}
+    const body = {name_task: task.name_task ,info_task: task.info_task ,point_task: task.point_task,status:5}
     return this.http.patch(config['baseURL'] + "/task/point/"+task.id  ,body,
     {headers: this.httpHeaders})
   }
 
   updateTasktoInProgress(task): Observable<any> {
-    const body = {status:2}
+    const body = {status:4}
     return this.http.patch(config['baseURL'] + "/task/"+task.id +"/" ,body,
     {headers: this.httpHeaders})
   }
