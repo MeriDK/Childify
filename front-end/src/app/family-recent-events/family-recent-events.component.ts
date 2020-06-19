6b104ed4436332192f463078535c272ab6b0d6c7
Ridiculous_P <yci.cipi@gmail.com>
2020-06-19 20:56:00 +0300

    Real time

--- front-end/src/app/family-recent-events/family-recent-events.component.ts
+++ front-end/src/app/family-recent-events/family-recent-events.component.ts
@@ -61,9 +61,22 @@ export class FamilyRecentEventsComponent implements OnInit {
       console.log('---------------------' + this.chooseImg(el['executor']['numIcon']));
       
       if (events) {
-        events.push({title: el['title'], status: this.parseCategory(el['status']), points: el['points'], time: el['time'], categoryImg: el['category'], imgUrl: this.chooseImg(el['executor']['numIcon'])});
+        events.push({
+          title: el['title'], 
+          status: this.parseCategory(el['status']), 
+          points: el['points'], 
+          time: new Date(el['time']).getHours() + ':' + new Date(el['time']).getMinutes(), 
+          categoryImg: el['category'], 
+          imgUrl: this.chooseImg(el['executor']['numIcon'])});
       } else {
-        events = [{title: el['title'], status: this.parseCategory(el['status']), points: el['points'], time: el['time'], categoryImg: el['category'], imgUrl: this.chooseImg(el['executor']['numIcon'])}]
+        events = [{
+          title: el['title'], 
+          status: this.parseCategory(el['status']), 
+          points: el['points'], 
+          time: new Date(el['time']).getHours() + ':' + new Date(el['time']).getMinutes(), 
+          categoryImg: el['category'], 
+          imgUrl: this.chooseImg(el['executor']['numIcon'])
+        }]
       }
     });
 
